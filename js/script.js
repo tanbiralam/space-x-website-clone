80f96d9f85db4f056af52d5dcf68f852a4c8aff0
Tanbir Alam <itzmetanbir@gmail.com>
2023-12-09 14:04:40 +0530

    update some css

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+
+
+btn.addEventListener('click', navToggle)
+
+function navToggle() {
+    btn.classList.toggle('open')
+    overlay.classList.toggle('overlay-show')
+    document.body.classList.toggle('stop-scrolling')
+}
